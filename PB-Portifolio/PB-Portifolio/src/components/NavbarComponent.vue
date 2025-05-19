2821ff37e72491a884a3e46b3d6f5c08dc09e257
Pedro Brene <pedrobrene22@gmail.com>
2025-05-19 13:38:38 -0300

    Update NavbarComponent.vue

--- PB-Portifolio/PB-Portifolio/src/components/NavbarComponent.vue
+++ PB-Portifolio/PB-Portifolio/src/components/NavbarComponent.vue
@@ -2,7 +2,6 @@
   <nav class="bg-black text-green-500 py-4 px-8 shadow-md rounded-b-lg">
     <div class="flex items-center max-w-7xl mx-auto">
 
-      <!-- Links de navegação à esquerda -->
       <ul class="flex gap-8 text-lg font-semibold flex-1">
         <li>
           <router-link
@@ -24,14 +23,12 @@
         </li>
       </ul>
 
-      <!-- Título centralizado -->
       <div class="flex flex-col items-center flex-none mx-auto">
         <h1 class="text-2xl font-extrabold tracking-wide text-green-500 drop-shadow-sm whitespace-nowrap">
           PORTFÓLIO PB
         </h1>
       </div>
 
-      <!-- Espaço vazio para equilibrar o layout -->
       <div class="flex-1"></div>
     </div>
   </nav>
